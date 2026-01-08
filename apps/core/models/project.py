1dfa9cae3e6c5f96184077ecad03f466d5ac91a8
Znooptokkie <oomenatilla@gmail.com>
2026-01-08 23:04:13 +0100

    MODEL: Added first model Project without relations yet

---
+++ apps/core/models/project.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=120)
+    subtitle = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    link = models.CharField(max_length=255, blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+    excerpt = models.TextField(blank=True, null=True)
+    github = models.CharField(max_length=255, blank=True, null=True)
+    featured = models.BooleanField(default=False)
+    in_progress = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "project"
+
+    def __str__(self):
+        return self.title
