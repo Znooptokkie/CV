88952bc775213940875574b814b2151f0d3fbb73
Znooptokkie <oomenatilla@gmail.com>
2026-01-18 21:07:18 +0100

    FRONTEND | HOMEPAGE: Third section done

--- apps/core/models/framework.py
+++ apps/core/models/framework.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Framework(models.Model):
     name = models.CharField(max_length=50, unique=True)
+    svg_url = models.CharField(max_length=255, unique=False, null=True)
 
     class Meta:
         db_table = "framework"
