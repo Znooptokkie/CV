b748696b7e7d71332cfb55a871ed466cf6042949
Znooptokkie <oomenatilla@gmail.com>
2026-01-22 00:10:26 +0100

    CONFIG V2: Changed config file 'dev.py' and 'prod.py' and 'base.py' so code is DRY

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,4 +1,16 @@
-from config.settings.base import *
+from config.settings.base import BASE_DIR as BASE_DIR
+from config.settings.base import INSTALLED_APPS as INSTALLED_APPS
+from config.settings.base import MIDDLEWARE as MIDDLEWARE
+from config.settings.base import ROOT_URLCONF as ROOT_URLCONF
+from config.settings.base import TEMPLATES as TEMPLATES
+
+DEBUG = True
+SECRET_KEY = "dev-secret-key"
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 
 # # ==============================
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,76 +1,8 @@
-# import os
-
-# from config.settings import base
-
-
-# # ==============================
-# # Installed apps, middleware, templates
-# # ==============================
-# INSTALLED_APPS = base.INSTALLED_APPS
-# MIDDLEWARE = base.MIDDLEWARE
-# TEMPLATES = base.TEMPLATES
-# ROOT_URLCONF = base.ROOT_URLCONF
-
-
-# # ==============================
-# # Basis settings
-# # ==============================
-# DEBUG = False
-# SECRET_KEY = base.SECRET_KEY
-# ALLOWED_HOSTS = ["atillaoomen.nl", "www.atillaoomen.nl"]
-
-
-# # ==============================
-# # Static files
-# # ==============================
-# STATIC_URL = "/static/"
-# STATIC_ROOT = os.getenv("DJANGO_STATIC_ROOT", base.BASE_DIR / "staticfiles")
-# STATICFILES_DIRS = [
-#         base.BASE_DIR / "static",
-# ]
-# # Mocht er een 500 server error zijn
-# # verander onderstaande in ...storage.StaticFilesStorage
-# STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
-
-
-# # ==============================
-# # Database
-# # ==============================
-# DATABASES = {
-#     "default": {
-#         "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.mysql"),
-#         "NAME": os.getenv("DB_NAME", base.DATABASES["default"]["NAME"]),
-#         "USER": os.getenv("DB_USER", base.DATABASES["default"]["USER"]),
-#         "PASSWORD": os.getenv("DB_PASSWORD", base.DATABASES["default"]["PASSWORD"]),
-#         "HOST": os.getenv("DB_HOST", "localhost"),
-#         "PORT": os.getenv("DB_PORT", "3306"),
-#     }
-# }
-
-
-# # ==============================
-# # Security
-# # ==============================
-# SECURE_BROWSER_XSS_FILTER = True
-# X_FRAME_OPTIONS = "DENY"
-# CSRF_COOKIE_SECURE = True
-# SESSION_COOKIE_SECURE = True
-# SECURE_SSL_REDIRECT = True
-
-# # HSTS (optioneel)
-# SECURE_HSTS_SECONDS = 3600
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_HSTS_PRELOAD = True
-
-
-# # ==============================
-# # Email / externe services
-# # ==============================
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
-# USE_EXTERNAL_API = True  # Flag voor eigen API calls of queues
-
-from config.settings.base import *
+from config.settings.base import BASE_DIR
+from config.settings.base import INSTALLED_APPS as INSTALLED_APPS
+from config.settings.base import MIDDLEWARE as MIDDLEWARE
+from config.settings.base import ROOT_URLCONF as ROOT_URLCONF
+from config.settings.base import TEMPLATES as TEMPLATES
 
 DEBUG = False
 
